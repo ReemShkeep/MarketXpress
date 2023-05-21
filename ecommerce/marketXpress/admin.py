1bd353bf2ffc081e8fd3b72c71ce56a5b7f1f35e
ReemShkeep <96814665+ReemShkeep@users.noreply.github.com>
2023-05-22 01:42:47 +0300

    Update admin.py

--- ecommerce/marketXpress/admin.py
+++ ecommerce/marketXpress/admin.py
@@ -1,9 +1,17 @@
 from django.contrib import admin
 # from .models import Product, Customer, Order, Category
 from .models import *
-admin.site.register(Product)
-admin.site.register(Customer)
-admin.site.register(Order)
-admin.site.register(Category)
+# tuble (), liist [], dictiomary {}
+class CustomCustomer(admin.ModelAdmin):
+  fieldsets = (
+    ["Customer's Information",{'fields':['name','phone','email','shippingAddress']}],
+    ["Order's Information",{'fields':['customer_order_date ','customer_product'] }]
+  )
+
+
+# admin.site.register(Product,)
+admin.site.register(Customer,CustomCustomer)
+# admin.site.register(Order)
+# admin.site.register(Category)
 
 # Register your models here.
