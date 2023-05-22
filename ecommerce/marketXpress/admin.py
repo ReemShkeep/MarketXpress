90f1b4bf27d64b9db003a1417272d1f447d2f0bc
Reem Shkeep <eng.reemshkeep@gmail.com>
2023-05-22 16:25:13 +0300

    adding other models and admin panel

--- ecommerce/marketXpress/admin.py
+++ ecommerce/marketXpress/admin.py
@@ -4,14 +4,56 @@ from .models import *
 # tuble (), liist [], dictiomary {}
 class CustomCustomer(admin.ModelAdmin):
   fieldsets = (
-    ["Customer's Information",{'fields':['name','phone','email','shippingAddress']}],
-    ["Order's Information",{'fields':['customer_order_date ','customer_product'] }]
+    ["Customer's Information",{'fields':['name','phone','age','email','shippingAddress']}],
+    ["Order's Information",{'fields':['customer_product'] }]
   )
+  list_display = ['name','phone','age','email','shippingAddress','is_adult']
+
+#   fieldsets = (
+#     ["Customer's Information",{'fields':['name','phone','email','shippingAddress']}],
+#     ["Order's Information",{'fields':['customer_order_date ','customer_product'] }]
+#   )
+class InlineProduct(admin.StackedInline):
+    model = Product
+    extra = 1
+
+class CustomCategory(admin.ModelAdmin):
+    inlines = [InlineProduct]
+    list_display = ['name','description']
+    search_fields = ['name']
+    list_filter = ['name']
+    list_per_page = 15
+    list_editable = ['description']
+    fieldsets = (
+        ["Category's Information", {'fields': ['name', 'description']}],
+    )
+
+class CustomOrder(admin.ModelAdmin):
+    list_display = ['order_date', 'customer_name', 'total_price']
+    search_fields = ['order_date', 'customer_name']
+    list_filter = ['order_date', 'customer_name']
+    list_per_page = 15
+    fieldsets = (
+        ["Order's Information", {'fields': ['order_date', 'customer_name', 'total_price','free_Shipping']}],
+    )
 
 
-# admin.site.register(Product,)
 admin.site.register(Customer,CustomCustomer)
+admin.site.register(Category,CustomCategory)
+admin.site.register(Product)
+admin.site.register(Order)
+admin.site.register(Brand)
+
+# admin.site.register(Product)
+# admin.site.register(Customer)
 # admin.site.register(Order)
 # admin.site.register(Category)
 
+
 # Register your models here.
+
+
+
+
+
+
--- ecommerce/marketXpress/models.py
+++ ecommerce/marketXpress/models.py
@@ -1,11 +1,22 @@
 from django.db import models
-# Product,Order,Customer,Category
+# Product,Order,Customer,Category,Brand
 
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=200, null=True)
+    description = models.CharField(max_length=200, null=True)
     def __str__(self):
         return self.name
+
+
+class Brand(models.Model):
+    name = models.CharField(max_length=200, null=True)
+    brand_category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True)
+    # image = models.ImageField(upload_to='brands/', null=True)
+    def __str__(self):
+        return self.name
+
+
 
 class Product(models.Model):
     name = models.CharField(max_length=200, null=True)
@@ -13,7 +24,10 @@ class Product(models.Model):
     description = models.CharField(max_length=200, null=True)
     digital = models.BooleanField(default=False,null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
-    product_category = models.ForeignKey(Category, null=True, on_delete=models.SET_NULL) #one to many relationship
+    product_category = models.ForeignKey(Category, null=True, on_delete=models.CASCADE) #one to many relationship
+    product_brand = models.ForeignKey(Brand, null=True, on_delete=models.CASCADE) #one to many relationship
+
+    
     # product_category = models.ForeignKey(Category, null=True, on_delete=models.CASCADE) #one to many relationship
     
     def __str__(self):
@@ -34,31 +48,70 @@ class Customer(models.Model):
     # fname = models.CharField(max_length=200, null=True)
     # lname = models.CharField(max_length=200, null=True)
     phone = models.CharField(max_length=200, null=True)
+    age= models.PositiveIntegerField(default=18)
     email = models.CharField(max_length=200, null=True)
-    ShippingAddress = models.CharField(max_length=200, null=True)
-    customer_order_date = models.DateTimeField(auto_now_add=True, null=True)
+    shippingAddress = models.CharField(max_length=255, null=True)
     customer_product = models.ManyToManyField(Product) #many to many relationship
+
+    def is_adult(self):
+        if self.age > 20:
+            return True
+        else:
+            return False
+    is_adult.short_description = 'Adult'
+    is_adult.boolean = True
+
+
 
     def __str__(self):
         return self.name
         # return self.fname+''+self.lname
 
 class Order(models.Model):          
-    customer = models.ForeignKey('Customer', null=True, on_delete=models.SET_NULL) #one to many relationship
+    customer = models.ForeignKey('Customer', null=True, on_delete=models.CASCADE) #one to many relationship
     date_ordered = models.DateTimeField(auto_now_add=True, null=True)
     complete = models.BooleanField(default=False, null=True, blank=False)
-    transaction_id = models.CharField(max_length=200, null=True)
+    # transaction_id = models.CharField(max_length=200, null=True)
+    order_product = models.ManyToManyField(Product) #many to many relationship
+    customer_order_date = models.DateTimeField(auto_now_add=True, null=True) 
+    # quantity = models.PositiveIntegerField()
 
-    
+    def total_cost(self):
+        total = 0
+        order_items = self.orderitem_set.all()
+        for item in order_items:
+            total += item.product.price * item.quantity
+        return total
+
+
+    def total_cost(self):
+        return self.price * self.quantity
+
+    def free_Shipping():
+        if self.total_cost() > 500:
+            return True
+        else:
+            return False
+
+    free_Shipping.short_description = 'FREE Shipping'        
+
+
+    def formatted_order_date(self):
+        return self.customer_order_date.strftime('%Y-%m-%d  @%H')
+
+
     def __str__(self):
-        return str(self.id)
-    
+        # return f"Order's ID {self.id} by {self.customer.name} on {self.customer_order_date}"     #date format till the seconds %H:%M:%S'
+        return f"Order's ID {self.id} by {self.customer.name} on {self.formatted_order_date()}" #date format till the day
+
+    # Total price
     @property
     def get_cart_total(self):
         orderitems = self.orderitem_set.all()
         total = sum([item.get_total for item in orderitems]) #list comprehension
         return total
     
+    # Total Items
     @property
     def get_cart_items(self):
         orderitems = self.orderitem_set.all()
